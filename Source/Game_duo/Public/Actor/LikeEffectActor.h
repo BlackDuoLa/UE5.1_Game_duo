2c58ec97ea7d04fbeb2e3ddac7f38355e95d3796
DuoLa <319188642@qq.com>
2024-08-06 10:49:25 +0800

    add Fire and tag
    把Fire持续效果给做了出来，添加了Tag,两种Tag添加模式，一种项目设置，一种表格添加。

--- Source/Game_duo/Public/Actor/LikeEffectActor.h
+++ Source/Game_duo/Public/Actor/LikeEffectActor.h
@@ -3,10 +3,51 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "Ability/LikeAttributeSet.h"
+#include "GameplayEffectTypes.h"
 #include "LikeEffectActor.generated.h"
 
 
+
+class AbilitySystemComponent;
 class UGameplayEffect;
+
+//UENUM(BlueprintType)
+//enum class EEffectApplicationPolicy
+//{
+//
+//	ApplyOnOverlap,
+//	ApplyOnEndOverlap,
+//
+//	DoNotApply
+//
+//};
+//
+//UENUM(BlueprintType)
+//enum class EEffectRemovalPolicy
+//{
+//
+//	RemoveOnEndOverlap,
+//	DoNotRemove
+//
+//};
+
+
+
+UENUM(BlueprintType)
+enum class EEffectApplicationPolicy : uint8
+{
+	ApplyOnOverlap UMETA(DisplayName = "Apply On Overlap"),
+	ApplyOnEndOverlap UMETA(DisplayName = "Apply On End Overlap"),
+	DoNotApply UMETA(DisplayName = "Do Not Apply")
+};
+
+UENUM(BlueprintType)
+enum class EEffectRemovalPolicy : uint8
+{
+	RemoveOnEndOverlap UMETA(DisplayName = "Remove On End Overlap"),
+	DoNotRemove UMETA(DisplayName = "Do Not Remove")
+};
+
 
 
 
@@ -26,15 +67,40 @@ protected:
 	UFUNCTION(BlueprintCallable)
 	void ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect>GameplayEffectClass);
 
+	UFUNCTION(BlueprintCallable)
+	void OnOverlap(AActor* TargetActor);
+
+	UFUNCTION(BlueprintCallable)
+	void OnEndOverlap(AActor* TargetActor);
+
+
+	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category = "Applied Effects")
+	bool bDestroyOnEffectRemoval = false;
+
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
 	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
+	EEffectApplicationPolicy InstantEffectApplicationPolicy = EEffectApplicationPolicy::DoNotApply;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
 	TSubclassOf<UGameplayEffect> DurationGameplayEffectClass;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
+	EEffectApplicationPolicy DurationEffectApplicationPolicy = EEffectApplicationPolicy::DoNotApply;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
+	TSubclassOf<UGameplayEffect> InfiniteGameplayEffectClass;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
+	EEffectApplicationPolicy InfiniteEffectApplicationPolicy = EEffectApplicationPolicy::DoNotApply;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
+	EEffectRemovalPolicy InfiniteEffectRemovalPolicy = EEffectRemovalPolicy::RemoveOnEndOverlap;
 
+	TMap<FActiveGameplayEffectHandle, UAbilitySystemComponent*>ActiveEffectHandles;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
+	float ActorLevel = 1.f;
 
 };
